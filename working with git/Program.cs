152e15333d34dd4800f7c35192498bde47d7a14a
Liel Ben-Or <liel4349@gmail.com>
2021-09-13 14:15:52 +0300

    Add project files.

---
+++ working with git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace working_with_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
